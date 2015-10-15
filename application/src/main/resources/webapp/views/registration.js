5a106adc589a79d9ca1a9cfd0a63c535b8913ee2
Henrik Sjöstrand <hencjo@gmail.com>
2015-10-15 14:49:22 +0200

    Added button to GUI to mark name tags as printed.

--- application/src/main/resources/webapp/views/registration.js
+++ application/src/main/resources/webapp/views/registration.js
@@ -1,9 +1,19 @@
 function RegistrationController($scope, $routeParams, $resource) {
 	var RegistrationsResource = $resource('gui/registrations/:id', {id: "@id"});
+	var NametagPrintedResource = $resource('gui/registrations/mark-as-printed', {});
     var BundlesResource = $resource('gui/buckets/', {}, {save: {method: 'PUT', isArray: true}});
 
     $scope.registration = RegistrationsResource.get({id: $routeParams.registrationId});
 	$scope.bundles = BundlesResource.query();
+
+	$scope.markNametagPrinted = function() {
+		NametagPrintedResource.save({registrations: [$scope.registration.id]}, function(d) {
+			$scope.alert = {
+				style: "success",
+				message: d.message
+			};
+		});
+	};
 
 	$scope.save = function() {
 		$scope.registration.$save(function() {

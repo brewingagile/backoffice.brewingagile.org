34fa98c302d98837e86f4d570cc1375ee2e6a82f
Henrik Sjöstrand <hencjo@gmail.com>
2015-10-15 11:06:44 +0200

    New service for getting a specific name tag.

--- application/src/main/java/org/brewingagile/backoffice/rest/gui/NameTagsRestService.java
+++ application/src/main/java/org/brewingagile/backoffice/rest/gui/NameTagsRestService.java
@@ -2,6 +2,7 @@ package org.brewingagile.backoffice.rest.gui;
 
 import argo.jdom.JsonRootNode;
 import fj.Ord;
+import fj.data.Option;
 import fj.data.Set;
 import org.brewingagile.backoffice.auth.AuthService;
 import org.brewingagile.backoffice.db.operations.RegistrationsSqlMapper;
@@ -13,11 +14,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.Connection;
+import java.util.UUID;
 
 import static argo.jdom.JsonNodeFactories.*;
 
@@ -38,14 +41,31 @@ public class NameTagsRestService {
 		this.registrationsSqlMapper = registrationsSqlMapper;
 	}
 
+//	curl -u admin:password -v http://localhost:9080/gui/nametags/
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response invoices(@Context HttpServletRequest request) throws Exception {
+	public Response all(@Context HttpServletRequest request) throws Exception {
 		authService.guardAuthenticatedUser(request);
 		try (Connection c = dataSource.getConnection()) {
 			return Response.ok(ArgoUtils.format(
 				array(registrationsSqlMapper.all(c).map(NameTagsRestService::json))
 			)).build();
+		}
+	}
+
+//	curl -u admin:password -v http://localhost:9080/gui/nametags/8e57b3d5-2a50-47a5-853f-bdb76cdbbb62
+
+	@GET
+	@Path("{registration_id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response one(@Context HttpServletRequest request, @PathParam("registration_id") UUID registrationId) throws Exception {
+		authService.guardAuthenticatedUser(request);
+		try (Connection c = dataSource.getConnection()) {
+			return registrationsSqlMapper.one(c, registrationId)
+				.map(r -> Response.ok(ArgoUtils.format(array(json(r)))))
+				.orSome(Response.status(Response.Status.NOT_FOUND))
+				.build();
 		}
 	}
 

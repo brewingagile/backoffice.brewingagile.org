8b465dca77545f2bef53f45d7273fd4d10d2e539
Henrik Sjöstrand <hencjo@gmail.com>
2015-10-19 15:17:56 +0200

    Improved dump-registrations.sh.

--- dump-registrations.sh
+++ dump-registrations.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh postgres@linode2  "psql -d brewingagile -c \"COPY registrations TO stdout DELIMITER ','\""
+ssh postgres@linode2  "psql -d brewingagile -c \"COPY (SELECT badge, ticket, participant_name, dietary_requirements FROM registrations ORDER BY badge DESC, ticket, participant_name) TO stdout WITH CSV\""

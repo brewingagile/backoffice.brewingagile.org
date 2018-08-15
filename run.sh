5a729b2a48c1ba5f425ccf55751be26a92cf9e6c
Henrik Sjöstrand <henrik@rebelalliance.se>
2018-08-15 15:02:12 +0200

    Check for ../backend.conf first, and use vanilla otherwise.

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-java -jar application/target/application-1-SNAPSHOT.jar backend.conf --dev application/src/main/resources/webapp/
+if [ -f ../backend.conf ]; then
+	BACKEND_CONF=../backend.conf
+else
+	BACKEND_CONF=./backend.conf
+fi 
+echo "Using $BACKEND_CONF"
+java -jar application/target/application-1-SNAPSHOT.jar "$BACKEND_CONF" --dev application/src/main/resources/webapp/

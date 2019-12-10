37c852766def4f10083098d06d10414f4313ad40
Henrik Sjöstrand <henrik@rebelalliance.se>
2019-12-10 15:13:22 +0100

    Adapted scripts to use gradle.

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ else
 	BACKEND_CONF=./backend.conf
 fi 
 echo "Using $BACKEND_CONF"
-java -jar application/target/application-1-SNAPSHOT.jar "$BACKEND_CONF" --dev application/src/main/resources/webapp/
+java -jar application/build/libs/application-all.jar "$BACKEND_CONF" --dev application/src/main/resources/webapp/

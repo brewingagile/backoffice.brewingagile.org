479d9bcc35dba48df0bc1baa89cc8fe96a4579e3
Henrik Sjöstrand <henrik@rebelalliance.se>
2019-12-18 20:48:55 +0100

    Use Base64-encoder bundled with java.

--- application/src/main/java/org/brewingagile/backoffice/utils/Http.java
+++ application/src/main/java/org/brewingagile/backoffice/utils/Http.java
@@ -1,9 +1,10 @@
 package org.brewingagile.backoffice.utils;
 
-import org.glassfish.jersey.internal.util.Base64;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Http {
 	public static String basic(String username, String password) {
-		return "Basic " + Base64.encodeAsString(username + ":" + password);
+		return "Basic " + Base64.getEncoder().encodeToString((username + ":" + password).getBytes(StandardCharsets.ISO_8859_1));
 	}
 }
